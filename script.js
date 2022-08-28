31375a01af379e8520d37eb27d835cf91ba0411a
Rakshit Gautam <rakshitgautam@192.168.1.7>
2022-08-29 01:16:47 +0530

    completed project

---
+++ script.js
@@ -0,0 +1,50 @@
+let apiQuotes = [];
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+getQuotes();
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+async function getQuotes() {
+  showLoadingSpinner();
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function newQuote() {
+  showLoadingSpinner();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author != null ? quote.author : "Unknown";
+  quote.text.length > 120
+    ? quoteText.classList.add("long-quote")
+    : quoteText.classList.remove("long-quote");
+  quoteText.textContent = quote.text;
+  removeLoadingSpinner();
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
